445df187616704ae2a7df6f8d94f6c1b3cbdd8bd
Jennifer Flores <jlflores@charter.net>
2019-01-01 15:42:07 -0600

    Add project files.

---
+++ FoodTruck1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodTruck1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
